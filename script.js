1f68dd85fed150942748e6f9f32c7ac8e37a8d1d
raulvictorvieira <raulvictorsv@gmail.com>
2021-09-16 23:31:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('form');
+const addList = document.querySelector('#addlist')
+const addButton = document.querySelector('#addbutton')
+const list = document.querySelector('#list')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+})
