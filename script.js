9527115b7c2ca077120a76e409201ea087c58d8f
raulvictorvieira <raulvictorsv@gmail.com>
2021-09-18 01:09:30 -0300

    funções de apagar e checked

--- script.js
+++ script.js
@@ -5,67 +5,71 @@ const result = document.querySelector('#result')
 const listArr = JSON.parse(localStorage.getItem("items")) || [];
 
 
-//função para salvar item da lista no localStorage
+//salvar item da lista no localStorage
 const saveItemsToLocaleStorage = () => {
     localStorage.setItem('items', JSON.stringify(listArr));
 }
 
-//função de popular as listas
+//popular a lista
 function showLists(clearList = false) {
 
-    //Limpar o conteúdo da div sempre que uma nova lista for inserida
-    if(clearList){
+    //Limpar o conteúdo da div sempre que uma novo item for inserido
+    if (clearList) {
         result.innerHTML = '';
     }
 
     //checar se o array existe e é maior que zero
-    if(listArr.length > 0) {
-        //11. utilizar o loop forEach para imprimir os nossos filmes
+    if (listArr.length > 0) {
+        //utilizar o loop forEach para imprimir as listas
         listArr.forEach((items) => {
             result.innerHTML += `<div>
-            <input type="checkbox" value:"${listArr.indexOf(items)}">
+            <input type="checkbox" onclick:"done()">
             <h3>
                 ${items}
             </h3>
-            <button class="erasebutton" onclick="erase()"><i class="far fa-trash-alt"></i></button>
+            <button class="erasebutton" onclick="erase() value="${listArr.indexOf(items)}"><i class="far fa-trash-alt"></i></button>
         </div>`
         });
-    }else {
-        result.innerHTML = 'Sem afazeres no momento...'
+    } else {
+        result.innerHTML = '<h3>Sem afazeres no momento...</h3>'
     }
 
 }
 
-function erase() {
-    const erasebutton = document.querySelector('.erasebutton')
-}
+//function done(event) {
+//    const index = event.target.id;
+//    console.log(index);
+//}
 
+//function erase(event) {
+ //   const index = event.target.value;
+//    listArr.splice(index, 1);
+//    saveItemsToLocaleStorage(listArr);
+//    showLists(true);
+//}
 
-
-
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
     event.preventDefault();
-    
-    const items = document.querySelector('#addlist').value;
-    const alertMessege = document.querySelector('.alert')
 
-    if(items === ""){
+    const items = document.querySelector('#addlist').value;
+    const alertMessege = document.querySelector('.alert');
+
+    if (items === "") {
         alertMessege.style = 'display: block;';
         alertMessege.innerHTML = '<i class="fas fa-pencil-alt"></i> Por favor, insira uma tarefa!';
-        setTimeout(function(){
+        setTimeout(function () {
             alertMessege.style = 'display: none;';
         }, 3000);
     } else {
         alertMessege.style = 'display: none;'
         listArr.push(items);
         form.reset();
-        saveItemsToLocaleStorage(listArr)
-        showLists(true)
-        //renderItems(listArr, true);
+        saveItemsToLocaleStorage(listArr);
+        showLists(true);
     }
-    
-})
 
-window.onload = function() {
+});
+
+window.onload = function () {
     showLists();
 }
