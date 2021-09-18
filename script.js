c39f3819f4f5d491309976733a756d560e328a1d
raulvictorvieira <raulvictorsv@gmail.com>
2021-09-17 22:49:39 -0300

    algumas funções adicionadas

--- script.js
+++ script.js
@@ -10,25 +10,62 @@ const saveItemsToLocaleStorage = () => {
     localStorage.setItem('items', JSON.stringify(listArr));
 }
 
+//função de popular as listas
+function showLists(clearList = false) {
+
+    //Limpar o conteúdo da div sempre que uma nova lista for inserida
+    if(clearList){
+        result.innerHTML = '';
+    }
+
+    //checar se o array existe e é maior que zero
+    if(listArr.length > 0) {
+        //11. utilizar o loop forEach para imprimir os nossos filmes
+        listArr.forEach((items) => {
+            result.innerHTML += `<div>
+            <input type="checkbox" value:"${listArr.indexOf(items)}">
+            <h3>
+                ${items}
+            </h3>
+            <button class="erasebutton" onclick="erase()"><i class="far fa-trash-alt"></i></button>
+        </div>`
+        });
+    }else {
+        result.innerHTML = 'Sem afazeres no momento...'
+    }
+
+}
+
+function erase() {
+    const erasebutton = document.querySelector('.erasebutton')
+}
+
 
 
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     
-    const item = document.querySelector('#addlist').value;
+    const items = document.querySelector('#addlist').value;
     const alertMessege = document.querySelector('.alert')
 
-    if(item === ""){
-        alertMessege.style = 'display: block;'
-        alertMessege.innerHTML = '<i class="fas fa-pencil-alt"></i> Por favor, insira uma tarefa!.' 
+    if(items === ""){
+        alertMessege.style = 'display: block;';
+        alertMessege.innerHTML = '<i class="fas fa-pencil-alt"></i> Por favor, insira uma tarefa!';
+        setTimeout(function(){
+            alertMessege.style = 'display: none;';
+        }, 3000);
     } else {
         alertMessege.style = 'display: none;'
-        listArr.push(item);
+        listArr.push(items);
         form.reset();
         saveItemsToLocaleStorage(listArr)
+        showLists(true)
         //renderItems(listArr, true);
-        console.log(listArr);
     }
     
 })
+
+window.onload = function() {
+    showLists();
+}
